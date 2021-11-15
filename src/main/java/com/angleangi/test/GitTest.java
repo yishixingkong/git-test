cbc514ec538ac19b8f6b3ccf12a217f038b84a23
Angleangi <Angleangi@163.com>
2021-11-15 10:50:29 +0800

    hot-fix one

--- src/main/java/com/angleangi/test/GitTest.java
+++ src/main/java/com/angleangi/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
